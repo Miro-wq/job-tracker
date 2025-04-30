33546843da887cbad7dc3a900235d72bcdeb3c0e
Miro <117939922+Miro-wq@users.noreply.github.com>
2025-04-30 13:06:04 +0300

    Update db.js

--- netlify/functions/utils/db.js
+++ netlify/functions/utils/db.js
@@ -3,6 +3,7 @@ let conn = null;
 
 exports.connectDB = async () => {
   if (conn) return conn;
+  console.log("▶️ MONGODB_URI =", process.env.MONGODB_URI);
   conn = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
